3dc9621cbab04b415411d8baeb240899318a72f9
Nagesh Singh <nst321888@gmail.com>
2022-06-22 12:54:59 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function randomNumber1(){
     var random = Math.random();
-    random = (Math.floor(100 * random)+1);
+    random = (Math.floor(90 * random)+10);
     console.log("number is : "+random);
     return(random);
 }
 
 function randomNumber2(){
     var random = Math.random();
-    random = (Math.floor(100 * random)+1);
+    random = (Math.floor(90 * random)+10);
     console.log("number is : "+random);
     return(random);
 }
 
-    document.firstElementChild.lastElementChild.style.backgroundColor= "darkcyan";
+function display(){
     document.querySelector("#p1").textContent = randomNumber1() ;
     document.querySelector("#p2").textContent = randomNumber2() ;
+}
+
+document.querySelector("button").addEventListner("click", display);
